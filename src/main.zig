0cafd3d1e3738fbbe6994a3489604e10ecb01cb4
jgold5 <jgold885@gmail.com>
2026-01-11 17:44:31 -0500

    initial sentinel functionality

--- src/main.zig
+++ src/main.zig
@@ -4,30 +4,93 @@ const linux = std.os.linux;
 
 pub fn main() !void {
     const in = try posix.inotify_init1(0);
-    std.debug.print("woke up {d}\n", .{in});
-    _ = try posix.inotify_add_watch(in, "/home/justin/Programming/sentinel/temp.txt", linux.IN.MODIFY);
+    const fp = "/home/justin/Programming/sentinel/temp";
+    const flags_to_watch = linux.IN.OPEN | linux.IN.CREATE | linux.IN.MODIFY | linux.IN.ATTRIB | linux.IN.DELETE | linux.IN.Q_OVERFLOW;
+    const wd = try posix.inotify_add_watch(in, fp, flags_to_watch);
     var fds = [_]posix.pollfd{
         .{ .fd = in, .events = posix.POLL.IN, .revents = 0 },
     };
-    const flags = [_]struct { flag: u32, name: []const u8 }{ .{ .flag = linux.IN.ACCESS, .name = "ACCESS" }, .{ .flag = linux.IN.MODIFY, .name = "MODIFY" }, .{ .flag = linux.IN.CLOSE_WRITE, .name = "CLOSE_WRITE" } };
+    const parent = std.heap.page_allocator;
+    var arena = std.heap.ArenaAllocator.init(parent);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    var watch_descriptors = std.AutoHashMap(i32, []const u8).init(allocator);
+    defer watch_descriptors.deinit();
+    var events = std.ArrayList(Event).init(allocator);
+    try watch_descriptors.put(wd, fp);
     _ = try posix.poll(&fds, -1);
     var buf: [1024]u8 = undefined;
-    const bytes_read = try posix.read(fds[0].fd, buf[0..]);
-    var i: usize = 0;
-    while (i < bytes_read) {
-        const raw = &buf[i];
-        const ev: *const linux.inotify_event = @ptrCast(@alignCast(raw));
-        for (flags) |f| {
-            std.debug.print("{h} {h}\n", .{ ev.mask, f.flag });
-            if (ev.mask & f.flag != 0) {
-                std.debug.print("mask used: {s}\n", .{f.name});
+    var count: usize = 0;
+    var new_wd: i32 = 0;
+    var joined: ?[]const u8 = undefined;
+    var bytes_read: usize = 0;
+    while (true) {
+        bytes_read = try posix.read(fds[0].fd, buf[0..]);
+        var i: usize = 0;
+        while (i < bytes_read) {
+            const raw = &buf[i];
+            const ev: *const linux.inotify_event = @ptrCast(@alignCast(raw));
+            const kind = EventKind.fromMask(ev.mask).?;
+            if (kind == EventKind.open) {} else {
+                std.debug.print("---------- COUNT {d}----------\n", .{count});
+                const base = watch_descriptors.get(ev.wd).?;
+                if ((kind == EventKind.create) and (ev.mask & linux.IN.ISDIR != 0)) {
+                    new_wd = try posix.inotify_add_watch(in, joined.?, flags_to_watch);
+                    try watch_descriptors.put(new_wd, joined.?);
+                    std.debug.print("\tDIR made {s}\n", .{joined.?});
+                } else {
+                    const file_name = ev.getName().?;
+                    joined = try std.fs.path.join(allocator, &.{ base, file_name });
+                    const to_add = Event{ .kind = kind, .abs_path = joined.? };
+                    try events.append(to_add);
+                    std.debug.print("\tEvent received of type {any} for file {s}\n", .{ to_add.kind, to_add.abs_path });
+                }
+                count += 1;
             }
+            i += @sizeOf(linux.inotify_event) + ev.len;
         }
-        std.debug.print("{any}\n", .{@TypeOf(ev)});
-        std.debug.print("wd {d}\n", .{ev.wd});
-        std.debug.print("mask {b}\n", .{ev.mask});
-        std.debug.print("cookie {d}\n", .{ev.cookie});
-        std.debug.print("len {d}\n", .{ev.len});
-        i += @sizeOf(linux.inotify_event) + ev.len;
+    }
+    for (events) |ev| {
+        ev.deinit(allocator);
     }
 }
+
+const EventKind = enum(u32) {
+    overflow = linux.IN.Q_OVERFLOW,
+    delete = linux.IN.DELETE,
+    create = linux.IN.CREATE,
+    modify = linux.IN.MODIFY,
+    attrib = linux.IN.ATTRIB,
+    open = linux.IN.OPEN,
+
+    fn fromMask(mask: u32) ?EventKind {
+        if (mask & @intFromEnum(EventKind.overflow) != 0) {
+            return EventKind.overflow;
+        }
+        if (mask & @intFromEnum(EventKind.delete) != 0) {
+            return EventKind.delete;
+        }
+        if (mask & @intFromEnum(EventKind.create) != 0) {
+            return EventKind.create;
+        }
+        if (mask & @intFromEnum(EventKind.modify) != 0) {
+            return EventKind.modify;
+        }
+        if (mask & @intFromEnum(EventKind.attrib) != 0) {
+            return EventKind.attrib;
+        }
+        if (mask & @intFromEnum(EventKind.open) != 0) {
+            return EventKind.open;
+        }
+        return null;
+    }
+};
+
+const Event = struct {
+    kind: EventKind,
+    abs_path: []const u8,
+
+    fn deinit(self: *Event, allocator: std.mem.Allocator) void {
+        allocator.free(self.abs_path);
+    }
+};
